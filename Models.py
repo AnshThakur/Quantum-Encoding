b170a7408e38557a5d3b8063ce5529ac1e115c1b
iacobo <47821203+iacobo@users.noreply.github.com>
2022-05-12 15:16:28 +0100

    Linting.

--- Models.py
+++ Models.py
@@ -4,38 +4,41 @@ from torch import nn
 
 
 class RNNClassifier(nn.Module):
-    
-    def __init__(self, input_dim, hidden_dim,out,device):
+
+    def __init__(self, input_dim, hidden_dim, out, device):
         super().__init__()
         self.hidden_dim = hidden_dim
         self.rnn = nn.RNNCell(input_dim, hidden_dim)  # RNN Cell
-        self.fc = nn.Linear(hidden_dim, out)            # fully connected layer: maps last hidden vector to model prediction
+        # fully connected layer: maps last hidden vector to model prediction
+        self.fc = nn.Linear(hidden_dim, out)
         self.activation = nn.Sigmoid()                # coz binary classification
         self.device = device
-    
+
     def forward(self, x):
 
         hidden = self.init_hidden(x)
-        
-        ############################# 
-        time_steps=x.shape[1]                 # shape of x is (batches,time_Steps,features)
-        
-        for i in range(0,time_steps):
-            inputs=x[:,i]                     # (batch,features) shape
+
+        #############################
+        # shape of x is (batches,time_Steps,features)
+        time_steps = x.shape[1]
+
+        for i in range(0, time_steps):
+            inputs = x[:, i]                     # (batch,features) shape
             hidden = self.rnn(inputs, hidden)
-            
-        out = self.fc(hidden)                 # take the hidden vector corresponding to last time step
+
+        # take the hidden vector corresponding to last time step
+        out = self.fc(hidden)
         ###########################
-        
+
         return self.activation(out)
-    
+
     def init_hidden(self, x):
         h0 = torch.zeros(x.size(0), self.hidden_dim)
         return h0.to(self.device)
 
 
 class LSTMClassifier(nn.Module):
-    
+
     def __init__(self, input_dim, hidden_dim, out, device):
         super().__init__()
         self.hidden_dim = hidden_dim
@@ -43,22 +46,23 @@ class LSTMClassifier(nn.Module):
         self.fc = nn.Linear(hidden_dim, out)
         self.activation = nn.Sigmoid()
         self.device = device
-    
+
     def forward(self, x):
         hidden, cell = self.init_hidden(x)
-        
+
         ############################
-        time_steps=x.shape[1]              #shape of x is (batches,time_Steps,features)
-        
+        time_steps = x.shape[1]  # shape of x is (batches,time_Steps,features)
+
         for i in range(time_steps):
-            inputs = x[:,i]                  # (batch,features) shape
-            hidden, cell = self.rnn(inputs, (hidden,cell))
-        
-        out = self.fc(hidden)              # take the hidden vector corresponding to last time step
+            inputs = x[:, i]                  # (batch,features) shape
+            hidden, cell = self.rnn(inputs, (hidden, cell))
+
+        # take the hidden vector corresponding to last time step
+        out = self.fc(hidden)
         #############################
-        
+
         return self.activation(out)
-    
+
     def init_hidden(self, x):
         h0 = torch.zeros(x.size(0), self.hidden_dim, device=self.device)
         c0 = torch.zeros(x.size(0), self.hidden_dim, device=self.device)
